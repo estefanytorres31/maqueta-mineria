f2052ececd8932e7bd275f520c21b1637912c567
Estefany Torres <estefanyt3101@gmail.com>
2026-08-22 13:19:40 -0500

    feat: implement MainLayout with integrated header, navigation, and content wrapper

--- src/renderer/layouts/MainLayout.tsx
+++ src/renderer/layouts/MainLayout.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from 'react'
 import Header from '../components/Header'
 import BottomNavigation from '../components/BottomNavigation'
+import { useNavigationStore } from '../stores/navigationStore'
 
 interface MainLayoutProps {
   children: ReactNode
 }
 
 export default function MainLayout({ children }: MainLayoutProps) {
+  const currentPage = useNavigationStore(s => s.currentPage)
+  
   return (
     <div className="w-full h-full flex flex-col">
       <Header />
       <main className="flex-1 overflow-hidden p-0.5 lg:p-0.5 xl:p-1.5 bg-industrial-900">
-        <div className="fade-in h-full w-full">
+        <div key={currentPage} className="fade-in h-full w-full">
           {children}
         </div>
       </main>
